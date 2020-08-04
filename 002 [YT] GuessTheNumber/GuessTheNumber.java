60f04c6d3712268bb015d0f4af6d82c230402ac7
Serge Kravchenko <0634380033@ukr.net>
2020-08-04 23:18:11 +0300

    [YT]

---
+++ 002 [YT] GuessTheNumber/GuessTheNumber.java
@@ -0,0 +1,32 @@
+/*
+Task Guess the Number
+Make a program in the console that will guess the number. 
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+		Scanner sc = new Scanner (System.in);
+		
+		do {
+			int count = 0;
+			int guess = -1;
+			int number = random.nextInt(10);
+			while (count < 3 && guess != number) {
+				System.out.println("Enter the number 0..9:");
+				guess = sc.nextInt ();
+				if (number != guess) {
+					System.out.println ("Your N is " + ((guess > number)? "greater" : "less"));
+					count++;
+			}
+		}
+	System.out.println	("You " + ((count == 3)? "lose!" : "WIN!"));
+	
+	System.out.println ("Repet game? Yes -Y , No - O");
+
+	} while (sc.next().equalsIgnoreCase("Y"));
+	}
+}

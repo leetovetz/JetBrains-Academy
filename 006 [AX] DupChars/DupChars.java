ce229cbf09bdf9d5eabca2709f5d0af19cb340ae
Serge Kravchenko <0634380033@ukr.net>
2020-08-10 22:22:10 +0300

    [AX]

---
+++ 006 [AX] DupChars/DupChars.java
@@ -0,0 +1,27 @@
+// The task. Create a program that will show duplicate characters in a sentence. 
+
+
+public class DupChars {
+
+	public static void main(String[] args) {
+		
+		String sentence = "How many duplicates are there?";
+		System.out.println(sentence);
+
+		String characters = "";
+		String duplicates = "";
+		for (int i = 0; i < sentence.length(); i++) {
+			String current = Character.toString(sentence.charAt(i));
+			if (characters.contains(current)) {
+				if (!duplicates.contains(current)) {
+					duplicates += current + ",";
+				}
+			}
+			characters += current;
+		}
+
+		System.out.println(duplicates);
+	}
+
+	
+}

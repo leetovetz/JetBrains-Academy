dfc7b26d88b1d0ed71d4d8952eabbcccd55bfc6d
Serge Kravchenko <0634380033@ukr.net>
2020-08-13 22:27:43 +0300

    [JB]

---
+++ 009 [JB] MethodWithArray/MethodWithArray.java
@@ -0,0 +1,22 @@
+// The task. To pass the method to an array.
+
+public class MethodWithArray {
+	
+	static int[] modifyArray(int[] array, int modifier) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = array[i] * modifier;
+		}
+		return array;
+	}
+
+
+	public static void main(String[] args) {
+		int[] myArray = {1, 2, 3, 4, 5};
+		
+		myArray = modifyArray(myArray, 5);
+		
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.print (myArray[i] + ", ");
+		}
+	}
+}

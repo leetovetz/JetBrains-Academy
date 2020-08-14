35db8d176d38702f87bd81b1f15d2b1c20b1e77c
Serge Kravchenko <0634380033@ukr.net>
2020-08-14 21:28:41 +0300

    [AX]

---
+++ 010 [AX] Factorial/Factorial.java
@@ -0,0 +1,22 @@
+// The challenge. 
+// Create a program that will read the number factorial. 
+// The factorial is 5! = 5 * (4 * 3 * 2 * 1) = 120
+
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		System.out.println(factorial(10));
+
+	}
+	public static int factorial(int n) {
+		if(n == 1) {
+			System.out.println("factorial(" + n + ") = 1");
+			return 1;
+		} else {
+			System.out.println("factorial(" + n + ") = " + n + " * factorial(" + (n - 1) + ")");
+			return n * factorial(n - 1);
+		}
+	}
+
+}

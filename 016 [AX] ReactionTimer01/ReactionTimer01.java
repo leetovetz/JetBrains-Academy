4a78c3f9998706d1adc52ed4dcfdb9caae8459d1
Serge Kravchenko <55243765+LeeTovetz@users.noreply.github.com>
2020-08-21 22:21:09 +0300

    Update ReactionTimer01.java

--- 016 [AX] ReactionTimer01/ReactionTimer01.java
+++ 016 [AX] ReactionTimer01/ReactionTimer01.java
@@ -9,7 +9,7 @@ public class ReactionTimer01 {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		System.out.println("3");
+	System.out.println("3");
         Thread.sleep(1000);
         System.out.println("2");
         Thread.sleep(1000);

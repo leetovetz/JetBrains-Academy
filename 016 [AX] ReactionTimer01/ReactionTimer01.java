d953a218f33079f80a5ff847bf78b04779399025
Serge Kravchenko <0634380033@ukr.net>
2020-08-21 22:19:42 +0300

    ReactionTimer

---
+++ 016 [AX] ReactionTimer01/ReactionTimer01.java
@@ -0,0 +1,34 @@
+/*
+ * The task. Create a simple timer that will measure your reaction. 
+ */
+
+import java.util.Scanner;
+
+
+public class ReactionTimer01 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("3");
+        Thread.sleep(1000);
+        System.out.println("2");
+        Thread.sleep(1000);
+        System.out.println("1");
+        Thread.sleep(1000);
+        
+        System.out.println("GO!");
+        
+        long startTime = System.currentTimeMillis();
+
+        Scanner scan = new Scanner(System.in);
+        scan.next();
+        
+        long stopTime = System.currentTimeMillis();
+        
+        long reactionTime = stopTime - startTime;
+        
+        System.out.println(reactionTime + " ms");
+        
+	}
+
+}

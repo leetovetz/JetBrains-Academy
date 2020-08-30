f31f3cfdcc7bfe3d3d86f6ac582e52ff06c2efaf
Serge Kravchenko <0634380033@ukr.net>
2020-08-30 21:00:41 +0300

    [AX]

---
+++ 018 [AX] Primes/Primes.java
@@ -0,0 +1,42 @@
+/*
+ * Task. Create a program that searches for prime numbers in a given range. 
+ * A prime number is a natural number with exactly two different natural dividers - 
+ * a unit and itself.
+ */
+
+
+import java.util.ArrayList;
+
+public class Primes {
+
+	public static void main(String[] args) {
+		
+		System.out.println(findPrimes(1, 100));
+		
+	}
+	
+	public static ArrayList<Integer> findPrimes(int start, int end) {
+		
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		
+		for(int n = start; n < end; n++) {
+			boolean prime = true;
+			
+			int i = 2; 
+			while(i <= n/2) {
+				if(n % i == 0) {
+					prime = false;
+					break;
+				}
+				i++;
+			}
+			
+			if(prime) {
+				primes.add(n);
+			}
+		}
+		
+		return primes;
+	}
+
+}

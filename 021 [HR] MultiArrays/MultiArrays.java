ad1be5494656b64533c251262116c77a489f0be6
Serge Kravchenko <0634380033@ukr.net>
2020-09-03 19:45:14 +0300

    [HR]

---
+++ 021 [HR] MultiArrays/MultiArrays.java
@@ -0,0 +1,31 @@
+/*
+ * Task. Introduce multidimensional array.
+ */
+
+
+public class MultiArrays {
+	
+	public static void twoDimentionalArrayDemo(int[][] iaa) {
+		System.out.println("Array contains: ");
+		for (int i = 0; i< iaa.length; i++) {
+			System.out.print(iaa[i].length + " elements: " );
+			for (int j = 0; j< iaa[i].length; j++) {
+				System.out.print(" " + iaa[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		int[][] iaaOne = {
+				{1, 2, 3, 4, 5},
+				{1, 2, 3, 4},
+				{1, 2, 3},
+				{1, 2},
+				{1}
+			};
+		twoDimentionalArrayDemo(iaaOne);
+
+	}
+
+}

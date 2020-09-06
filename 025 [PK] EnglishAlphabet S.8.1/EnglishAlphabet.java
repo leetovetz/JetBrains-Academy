1e2706dadc97afe43675ba7e375265a467fa1edb
Serge Kravchenko <0634380033@ukr.net>
2020-09-06 20:35:55 +0300

    [PK]

---
+++ 025 [PK] EnglishAlphabet S.8.1/EnglishAlphabet.java
@@ -0,0 +1,24 @@
+/*
+ * Task. S.8.1  
+ * Display all letters of the English alphabet on the screen.
+ */
+
+public class EnglishAlphabet {
+
+	public static void main(String[] args) {
+		printEnglishAlphabet();
+
+	}
+	private static void printEnglishAlphabet() {
+        printEnglishAlphabet('A', 'Z');
+        System.out.println(" ");
+        printEnglishAlphabet('a', 'z');
+        
+    }
+
+    private static void printEnglishAlphabet(final char from, final char to) {
+        for (char ch = from; ch <= to; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+}

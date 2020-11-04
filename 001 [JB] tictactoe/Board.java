6c8d829495becda2e8fd63be592fe296e02673ef
Serge Kravchenko <55243765+LeeTovetz@users.noreply.github.com>
2020-11-04 22:26:51 +0200

    Update Board.java

--- 001 [JB] tictactoe/Board.java
+++ 001 [JB] tictactoe/Board.java
@@ -1,26 +1,21 @@
 package tictactoe;
 
 public class Board {
-	
+
 	private String[][] array;
 
-	 public Board() {
-	        array = new String[][]{
-	                {"X","O","X"},
-	                {"O","X","O"},
-	                {"X","X","O"}
-	        };
-	    }
-	 
-	    public void print() {
-	        for (int r = 0; r < 3; r++) {
-	            for (int c = 0; c < 3; c++) {
-	                if (c < 2)
-	                    System.out.print(array[r][c] + " ");
-	                else
-	                    System.out.println(array[r][c]);
-	            }
-	        }
-	    }
+	public Board() {
+		array = new String[][] { { "X", "O", "X" }, { "O", "X", "O" }, { "X", "X", "O" } };
 	}
 
+	public void print() {
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 3; c++) {
+				if (c < 2)
+					System.out.print(array[r][c] + " ");
+				else
+					System.out.println(array[r][c]);
+			}
+		}
+	}
+}

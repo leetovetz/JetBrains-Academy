bd93be96fb2e379db6b06ae7cf4d00bd8e617797
Serge Kravchenko <55243765+LeeTovetz@users.noreply.github.com>
2020-11-11 22:59:09 +0200

    Update MinimalValue.java

--- 007 [SA] MinimalValue/MinimalValue.java
+++ 007 [SA] MinimalValue/MinimalValue.java
@@ -3,20 +3,18 @@
 public class MinimalValue {
 
 	public static void main(String[] args) {
-		int [] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+		int[] array = new int[] { 64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36 };
 
 		int minValue = array[0];
 		int minindex = 0;
-		
-		
+
 		for (int i = 1; i < array.length; i++) {
-            if (array[i] < minValue) {
-            	minValue = array[i];
-            	minindex = i;
-            }
-        }
+			if (array[i] < minValue) {
+				minValue = array[i];
+				minindex = i;
+			}
+		}
 		System.out.println("min value = " + minValue);
 		System.out.println("min index = " + minindex);
 	}
-
 }

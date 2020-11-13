a0d2b9574a94bcf5f70c2e7c9789a4ef081918b6
Serge Kravchenko <55243765+LeeTovetz@users.noreply.github.com>
2020-11-13 22:33:08 +0200

    Update Palindromes.java

--- 011 [AX] Palindromes/Palindromes.java
+++ 011 [AX] Palindromes/Palindromes.java
@@ -4,35 +4,31 @@
  * Ex: mom, dad, race car, nurses run
  */
 
-
-
 public class Palindromes {
 
 	public static void main(String[] args) {
-		
+
 		String original = "nurses run";
 		original = original.replace(" ", "");
-		
+
 		String reverse = "";
-		for(int i = original.length() - 1; i >= 0; i--) {
+		for (int i = original.length() - 1; i >= 0; i--) {
 			reverse += original.charAt(i);
 			System.out.println(reverse);
 		}
-		
+
 		boolean palindrome = true;
-		for(int i = 0; i < original.length(); i++) {
-			if(original.charAt(i) != reverse.charAt(i)) {
+		for (int i = 0; i < original.length(); i++) {
+			if (original.charAt(i) != reverse.charAt(i)) {
 				palindrome = false;
 			}
 		}
-		
-		if(palindrome) {
+
+		if (palindrome) {
 			System.out.println("PALINDROME!");
 		} else {
 			System.out.println("Not a palindrome!");
-		
-		
+
 		}
 	}
-
 }

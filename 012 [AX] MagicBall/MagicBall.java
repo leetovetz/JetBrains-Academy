9d4e1509c5768deb86da870197311811c56590b1
Serge Kravchenko <55243765+LeeTovetz@users.noreply.github.com>
2020-11-14 22:37:02 +0200

    Update MagicBall.java

--- 012 [AX] MagicBall/MagicBall.java
+++ 012 [AX] MagicBall/MagicBall.java
@@ -1,6 +1,5 @@
 //The task. To create a program that answers a question. 
 
-
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,28 +7,25 @@ public class MagicBall {
 
 	public static void main(String[] args) {
 		System.out.println("Enter your question");
-		
+
 		Scanner scan = new Scanner(System.in);
 		scan.next();
 		scan.close();
-		
+
 		Random rand = new Random();
 		int r = rand.nextInt(5);
 		System.out.println(r);
-		
-		if(r == 0) {
+
+		if (r == 0) {
 			System.out.println("As I see it, yes.");
-		} else if(r == 1) {
+		} else if (r == 1) {
 			System.out.println("Signs point to yes.");
-		} else if(r == 2) {
+		} else if (r == 2) {
 			System.out.println("Reply hazy, try again.");
-		} else if(r == 3) {
+		} else if (r == 3) {
 			System.out.println("Don't count on it.");
-		} else if(r == 4) {
+		} else if (r == 4) {
 			System.out.println("Outlook not so good.");
 		}
-
-		
 	}
-
 }

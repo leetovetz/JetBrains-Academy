df88cfee1ade2ba99d234170070a33d37bf4aaea
Serge Kravchenko <55243765+LeeTovetz@users.noreply.github.com>
2020-11-16 23:02:25 +0200

    Update Anagrams.java

--- 014 [YT] Anagrams/Anagrams.java
+++ 014 [YT] Anagrams/Anagrams.java
@@ -3,62 +3,60 @@
 *  All non-letter symbols should stay on the same places:
 *  E.g. "a1bcd efg!h" => "d1cba hgf!e"
 *  Use Latin alphabet for test only.
-  */
+*/
 
 import java.util.Scanner;
 
 public class Anagrams {
-	
-		public static void Reverse(String text) {			
+
+	public static void Reverse(String text) {
 
 		int l = 0;
-		
-		int r = text.length()-1;	
-		
+
+		int r = text.length() - 1;
+
 		char[] array = text.toCharArray();
-		
+
 		while (l < r) {
-	
+
 			if (Character.isLetter(array[r]) == false) {
 				r--;
-				}
-						
-				else if (Character.isLetter(array[l]) == false) {
-				l++;
-				}
-						
-				else {
-							
-					char a = array[r];
-				
-					array[r] = array[l];
-				
-					array[l] = a;
-					
-					l++;
-				
-					r--;
-					}
-				}
-		
-		for(int i = 0; i<=array.length-1; i++) {
-			System.out.print(array[i]);
 			}
-		
-		System.out.print(" ");
+
+			else if (Character.isLetter(array[l]) == false) {
+				l++;
+			}
+
+			else {
+
+				char a = array[r];
+
+				array[r] = array[l];
+
+				array[l] = a;
+
+				l++;
+
+				r--;
+			}
 		}
 
-				
-public static void main (String... args) {	
-			
-	Scanner scan = new Scanner(System.in);	
-				
-	while (scan.hasNext()) {		
-						
-		Reverse(scan.next());
-		}	
-		System.out.print("Why this text does not appear?");	
-		scan.close();				
+		for (int i = 0; i <= array.length - 1; i++) {
+			System.out.print(array[i]);
 		}
-		
+
+		System.out.print(" ");
+	}
+
+	public static void main(String... args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		while (scan.hasNext()) {
+
+			Reverse(scan.next());
+		}
+		System.out.print("Why this text does not appear?");
+		scan.close();
+	}
 }

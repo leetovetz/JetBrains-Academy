9ddcad4316830dcf02df11c27180f56935ae2f99
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2021-01-03 23:02:17 +0200

    Basics Task1

---
+++ 032 [EP] JavaIntroOnlineEPAM/001 Basics_of_software_code_development/Task1.java
@@ -0,0 +1,25 @@
+/*
+ * Find the value of the function: z = ( (a – 3 ) * b / 2) + c
+ */
+
+package Basics;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter three numbers a, b, c.");
+
+		Scanner scanner = new Scanner(System.in);
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		double c = scanner.nextDouble();
+
+		System.out.println(function(a, b, c));
+	}
+
+	private static double function(double a, double b, double c) {
+		return ((a - 3) * b / 2) + c;
+	}
+}

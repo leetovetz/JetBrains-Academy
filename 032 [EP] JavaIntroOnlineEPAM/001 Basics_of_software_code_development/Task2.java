608768833ef888b09aca65cdf7a72b6f1b800fba
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2021-01-04 14:25:16 +0200

    new edits

--- 032 [EP] JavaIntroOnlineEPAM/001 Basics_of_software_code_development/Task2.java
+++ 032 [EP] JavaIntroOnlineEPAM/001 Basics_of_software_code_development/Task2.java
@@ -1,25 +1,28 @@
 /*
- * Find the value of the function: z = ( (a – 3 ) * b / 2) + c
+ * Calculate the value of the expression using the formula (all variables take real values):
+ * ((b + âˆš(b^2 + 4ac))/ 2a) - a^3 * c + b^-2
  */
 
 package Basics;
 
 import java.util.Scanner;
 
-public class Task1 {
+public class Task2 {
 
 	public static void main(String[] args) {
-		System.out.println("Enter three numbers a, b, c.");
+		System.out.println("Please enter any three numbers a, b, c ...");
 
 		Scanner scanner = new Scanner(System.in);
 		double a = scanner.nextDouble();
 		double b = scanner.nextDouble();
 		double c = scanner.nextDouble();
 		scanner.close();
+
 		System.out.println(function(a, b, c));
 	}
 
 	private static double function(double a, double b, double c) {
-		return ((a - 3) * b / 2) + c;
+		return (b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) - Math.pow(a, 3) * c + Math.pow(b, -2);
+
 	}
 }
